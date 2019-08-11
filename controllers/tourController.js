decce8682fc68bfbcab6e6dbdc5acab062dc10b1
Taliaa <taliaa10@ymail.com>
2019-08-10 19:11:14 -0500

    imp param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,17 @@ tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'invalid ID'
+    });
+  }
+  next();
+};
+
 // Get the data in the JSON file and create a new JSON
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -21,13 +32,6 @@ exports.getTour = (req, res) => {
 
   const id = parseInt(req.params.id);
   const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'invalid ID'
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -59,12 +63,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'invalid ID'
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -74,12 +72,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'invalid ID'
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null
